21334d2c98c0bd0f5cb19629031ead0ea4738c4f
iamseungwoo <mongoose@yu.ac.kr>
2022-05-27 20:23:54 +0900

    django assignment login, logout, signup

--- Django/accounts/views.py
+++ Django/accounts/views.py
@@ -1,5 +1,20 @@
+from django.shortcuts import render
+
+def login(request):
+    return
+
+
+def logout(request):
+    return
+
+def signup(request):
+    return
+
+
+
 from django.shortcuts import redirect, render
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
     if request.method == "POST":
@@ -20,7 +35,16 @@ def logout(request):
     return redirect('home')
 
 def signup(request):
-    return
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+            )
+            auth.login(request, user)
+            return redirect('home')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
 
 
 
